7ceed5c3acbbadf9776b6b0779ceb458f251d18e
vashist01 <rajesh.vashist.01@gmail.com>
2026-09-11 08:07:31 +0530

    kuberts examples

---
+++ kubernetsDeployement/src/main/java/com/kubernets/KubernetsDeployementApplication.java
@@ -0,0 +1,13 @@
+package com.kubernets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetsDeployementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetsDeployementApplication.class, args);
+	}
+
+}
