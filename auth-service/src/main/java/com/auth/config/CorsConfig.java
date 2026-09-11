d69618f302c9923fe918b45369668927735ebf8d
vashist01 <rajesh.vashist.01@gmail.com>
2026-09-11 11:44:48 +0530

    update all auth-service file in kubernets and mysql

--- auth-service/src/main/java/com/auth/config/CorsConfig.java
+++ auth-service/src/main/java/com/auth/config/CorsConfig.java
@@ -2,13 +2,8 @@ package com.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 @Configuration
 public class CorsConfig {
     @Bean
